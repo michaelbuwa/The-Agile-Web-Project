5b341c7be215b07dfcf9ba91f9e582a9fb4e03d5
Shinetopia <shinetopiaofficial@outlook.com>
2025-05-03 21:21:02 +0800

    Fixed css and js links for flask, rearranged jinja templates to render page layouts correctly

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html", title="Colour Mania", include_google_fonts=True, include_bootstrap=True)
+	return render_template('index.html', title='Colour Mania', include_google_fonts=True, include_bootstrap=True)
 @app.route('/sign-up')
 def sign_up():
-	return render_template("sign-up.html", title="Sign Up", include_bootstrap=True)
+	return render_template('sign-up.html', title='Sign Up', include_bootstrap=True)
 @app.route('/upload')
 def upload():
-	return render_template("upload.html", title="Colour Differentiation Test", include_navbar=True, body_class="upload-page")
+	return render_template('upload.html', title='Colour Differentiation Test', include_navbar=True, body_class='upload-page')
 @app.route('/visualise')
-def upload():
-	return render_template("visualise.html", title="Data Visualisation", include_navbar=True)
+def visualise():
+	return render_template('visualise.html', title='Data Visualisation', include_navbar=True)
 @app.route('/share')
 def share():
-	return render_template("share.html", title="Share", include_navbar=True, include_bootstrap=True)
+	return render_template('share.html', title='Share', include_navbar=True, include_bootstrap=True)
