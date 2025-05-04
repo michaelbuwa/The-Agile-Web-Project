5e108a8f577e601d0231f902a36e0b6262305899
MaxMacey <maxmacey1@gmail.com>
2025-05-05 01:42:01 +0800

    Avoided using current_app so no context is required at import to avoid an out of context error

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,23 @@
 from flask import render_template
-from app import app
+from app import application 
 
-@app.route('/')
-@app.route('/index')
+@application.route('/')
+@application.route('/index')
 def index():
-	return render_template('index.html', title='Colour Mania', include_google_fonts=True, include_bootstrap=True)
-@app.route('/sign-up')
+    return render_template('index.html', title='Colour Mania', include_google_fonts=True, include_bootstrap=True)
+
+@application.route('/sign-up')
 def sign_up():
-	return render_template('sign-up.html', title='Sign Up', include_bootstrap=True)
-@app.route('/upload')
+    return render_template('sign-up.html', title='Sign Up', include_bootstrap=True)
+
+@application.route('/upload')
 def upload():
-	return render_template('upload.html', title='Colour Differentiation Test', include_navbar=True, body_class='upload-page')
-@app.route('/visualise')
+    return render_template('upload.html', title='Colour Differentiation Test', include_navbar=True, body_class='upload-page')
+
+@application.route('/visualise')
 def visualise():
-	return render_template('visualise.html', title='Data Visualisation', include_navbar=True)
-@app.route('/share')
+    return render_template('visualise.html', title='Data Visualisation', include_navbar=True)
+
+@application.route('/share')
 def share():
-	return render_template('share.html', title='Share', include_navbar=True, include_bootstrap=True)
+    return render_template('share.html', title='Share', include_navbar=True, include_bootstrap=True)
