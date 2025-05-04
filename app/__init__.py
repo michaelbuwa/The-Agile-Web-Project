0c10538c6ff0a7cc01604fe826b06e89c9a65c8b
MaxMacey <maxmacey1@gmail.com>
2025-05-04 16:48:41 +0800

    added the necessary flask stuff to __init__.py to create the SQLAlchemy object db and commented out the existing template from labs for later

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
 """
 Template from labs to be altered later
-"""
+
 from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+"""
+
+"""
+Database Flask integration
+"""
+from flask import flask
+from flask import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+application = Flask(__name__)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
+
+from app import routes, models
