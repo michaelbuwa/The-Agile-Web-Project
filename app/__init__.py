aca782b0c9bdd9b9df193c2c73ae6b3cec0dbe64
MaxMacey <maxmacey1@gmail.com>
2025-05-04 22:24:06 +0800

    fixed typos in __init__.py and config.py which were preventing imports from working properly on start up

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ from app import routes
 """
 Database Flask integration
 """
-from flask import flask
-from flask import Migrate
+from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 
--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-basedir = os.path.abstract(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 default_database_location = 'sqlite:///'+ os.path.join(basedir,'app.db')
 
 class Config:
